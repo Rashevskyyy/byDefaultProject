2c998bf6aa433943ee9e6bc497e3e33925c0af6d
Rashevskyyy <keepcalm312000@gmail.com>
2021-05-20 22:58:04 +0300

    add routes

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,16 @@ const mongoose = require('mongoose')
 
 const app = express();
 
+app.use('/api/auth', require('./routes/auth.routes'))
+
 const PORT = config.get('port') || 5000
 
 async function start() {
     try {
     await mongoose.connect(config.get('mongoUri'), {
-
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
     })
     } catch (e) {
         console.log('Server Error', e.message)
