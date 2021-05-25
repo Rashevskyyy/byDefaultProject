369c8c21550d06dea984bad7c640900d63ca82e0
Anastasiia <khramovanastasiia@gmail.com>
2021-05-25 18:00:30 +0300

    button create with some change

--- frontend/logic/helpers/btnCreate.js
+++ frontend/logic/helpers/btnCreate.js
@@ -1,10 +1,31 @@
 const create = document.getElementById('create');
-const closeCreate = document.getElementById('close-create');
+// const block = document.getElementById('table');
+// const closeCreate = document.getElementById('close-create');
 
 create.addEventListener('click', () =>{
-    document.getElementById("modal-window-create").classList.toggle("show-modal-btns");
+    document.getElementById("modal-window-create").classList.toggle("show-create");
 })
 
-closeCreate.addEventListener('click', () =>{
-    document.getElementById("modal-window-create").classList.remove("show-modal-btns");
-})
+// closeCreate.addEventListener('click', () =>{
+//     document.getElementById("modal-window-create").classList.remove("show-modal-btns");
+// })
+
+
+// create.addEventListener('click', addFunction);
+
+// function addFunction(){
+//     let blockElem = document.createElement('div');
+//     blockElem.className = 'table__main row';
+//     blockElem.innerHTML =  `
+//     <div class="table__main__item  small"></div>
+//     <div class="table__main__item"></div>
+//     <div class="table__main__item"></div>
+//     <div class="table__main__item small"></div>
+//     <div class="table__main__item small"></div>
+//     <div class="table__main__item"></div>
+//     <div class="table__main__item"></div>
+//     <div class="table__main__item"><div class="delete-persons" data-del="delete"></div></div>
+//         `
+//     block.prepend(blockElem);
+    
+// }
