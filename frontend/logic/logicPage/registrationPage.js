504b559b6ca9150554383165cf1669c84687f736
Anastasiia <khramovanastasiia@gmail.com>
2021-05-29 18:55:41 +0300

    change

--- frontend/logic/logicPage/registrationPage.js
+++ frontend/logic/logicPage/registrationPage.js
@@ -3,37 +3,38 @@ import '../../scss/createAccount.scss';
 import '../helpers/registPassword';
 import {REGURL, createOptions, postRequest} from "../helpers/requests"
 import {validateLogin, validatePassword} from '../logicPage/validation/validation'
+const loginInput = document.querySelector('#UserNameInput_SignUp');
+const passwordInput = document.querySelector('#PasswordInput_SignUp');
+const passwordConfirm = document.getElementById('password-confirm');
+const signButton = document.querySelector('#SignUpButton');
+const fieldsForErrror = document.getElementById('error');
 const regInit = () => {
-    const loginInput = document.querySelector('#UserNameInput_SignUp');
-    const passwordInput = document.querySelector('#PasswordInput_SignUp');
-    const passwordConfirm = document.getElementById('password-confirm');
-    const signButton = document.querySelector('#SignUpButton');
-    const fieldsForErrror = document.getElementById('error');
+  
     signButton.addEventListener("click", () => {
         const user = {
             login: loginInput.value,
             password: passwordInput.value,
             passwordConfirm: passwordConfirm.value
         }
-        let checkPassword = validatePassword(user.password) === validatePassword(user.passwordConfirm);
-        const regOptions = createOptions(user);
-        if(validateLogin(user.login) && checkPassword){
+        const regOptions = createOptions(user); 
+        if(validateLogin(user.login) && validatePassword(user.password)){
             postRequest(REGURL, regOptions).then((data) => {
-                switch (data.message) {
-                    case "Пользователь создан": window.location.pathname = "./autorizationPage.html"
-                    break;
-                    case "Такой пользователь уже существует":
-                        fieldsForErrror.innerHTML = "User already exists";
-                    break;
-                    case "Что-то пошло не так, попробуйте снова": console.log('Ошибка сервера');
-                    break;
+                if(data.message === "Что-то пошло не так, попробуйте снова"){
+                    console.log('Ошибка сервера');
+                } else if(data.message === "Такой пользователь уже существует"){
+                    fieldsForErrror.innerHTML = "User already exists";
+                } else if(user.password !== user.passwordConfirm){
+                    fieldsForErrror.innerHTML = "Passwords are different";
+                }else{
+                    window.location.pathname = "./autorizationPage.html";
                 }
-        }).catch(() => {
-            fieldsForErrror.innerHTML = "Server is down, try again later";
-        })
+            }).catch(() => {
+                fieldsForErrror.innerHTML = "Server is down, try again later";
+            })
         } else{
             fieldsForErrror.innerHTML = "Data entered incorrectly";
-         }
+        }
     })
 }
-regInit()
+regInit();
+
--- frontend/logic/logicPage/validation/validation.js
+++ frontend/logic/logicPage/validation/validation.js
@@ -1,25 +1,25 @@
 export const validateLogin = (login) => {
-  if (!login) {
-    return false;
-  }
-  const reg = new RegExp(/^[a-z0-9]{3,25}/, "i");
-    if (!reg.test(login)) {
+    if (!login) {
       return false;
     }
-  return true;
+    const reg = new RegExp(/^[a-z0-9]{3,25}/, "i");
+      if (!reg.test(login)) {
+        return false;
+      }
+    return true;
 };
-
+  
 export const validatePassword = (password) => {
-  if (!password) {
-    return false;
-  }
-  const reg = new RegExp(/^[a-z0-9]{6,25}/, "i");
-  if (!reg.test(password)) {
-    return false;
-  }
-  return true;
+    if (!password) {
+      return false;
+    }
+    const reg = new RegExp(/^[a-z0-9]{6,25}/, "i");
+    if (!reg.test(password)) {
+      return false;
+    }
+    return true;
 };
-
+  
 // export const validateEmail = (email) => {
 //     if (!email) {
 //       return false;
