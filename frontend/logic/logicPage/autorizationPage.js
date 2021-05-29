a268de5454c5439fe41b699dbb1416c4780e3b24
Anastasiia <khramovanastasiia@gmail.com>
2021-05-29 16:26:33 +0300

    validation om autorization and registration page

--- frontend/logic/logicPage/autorizationPage.js
+++ frontend/logic/logicPage/autorizationPage.js
@@ -1,27 +1,35 @@
 import '../../scss/autorization.scss';
-import '../header/localization';
+// import '../header/localization';
 // import '../helpers/showPassword';
 import '../helpers/singInPassword'
 import {LOGURL, createOptions, postRequest} from "../helpers/requests"
 import {setCookie} from "../helpers/general"
+import {validateLogin, validatePassword} from '../logicPage/validation/validation'
 
 const logInit = () => {
     const loginInput = document.querySelector('#LoginInput_SignIn');
     const passwordInput = document.querySelector('#PasswordInput_SignIn');
     const signInButton = document.querySelector('#SignInButton');
-
+    const fieldsForErrror = document.getElementById('error');
     signInButton.addEventListener("click", () => {
-
-        const user = {login: loginInput.value, password: passwordInput.value}
+        const user = {
+            login: loginInput.value,
+            password: passwordInput.value
+        }
         const regOptions = createOptions(user)
         postRequest(LOGURL, regOptions).then((data) => {
-            if (data.token) {
-                setCookie("token", data.token)
-                window.location.pathname = "./mainPage.html"
+            if(validateLogin(user.login) && validatePassword(user.password)){
+                if (data.token) {
+                    setCookie("token", data.token)
+                    window.location.pathname = "./mainPage.html"
+                } else{
+                    fieldsForErrror.innerHTML = "This user does not exist";
+                }
+            } else{
+                fieldsForErrror.innerHTML = "Data entered incorrectly";
             }
         }).catch(() => {
-            // Здесь делать отрисовку ошибки что сервер не работает, попробуйте позже
-            console.log('Умер')
+            fieldsForErrror.innerHTML = "Server is down, try again later";
         })
     })
 
--- frontend/logic/logicPage/registrationPage.js
+++ frontend/logic/logicPage/registrationPage.js
@@ -1,36 +1,40 @@
 import '../../scss/createAccount.scss';
-import '../header/localization';
+// import '../header/localization';
 import '../helpers/registPassword';
 import {REGURL, createOptions, postRequest} from "../helpers/requests"
-
+import {validateLogin, validatePassword} from '../logicPage/validation/validation'
 const regInit = () => {
     const loginInput = document.querySelector('#UserNameInput_SignUp');
     const passwordInput = document.querySelector('#PasswordInput_SignUp');
-    // confirm node
+    const passwordConfirm = document.getElementById('password-confirm');
     const signButton = document.querySelector('#SignUpButton');
-
+    const fieldsForErrror = document.getElementById('error');
     signButton.addEventListener("click", () => {
-        // const validation = VALIDATE(логин пароль конфирм пароль)
-        // если валидация прошла выполняется что внизу
-
-        const user = {login: loginInput.value, password: passwordInput.value}
-        const regOptions = createOptions(user)
-        postRequest(REGURL, regOptions).then((data) => {
-            switch (data.message) {
-                case "Пользователь создан": window.location.pathname = "./autorizationPage.html"
-                break;
-                case "Такой пользователь уже существует":
-                    // Отрисовать ошибку пользователь существует
-                    console.log('Пользователь существует');
-                break;
-                case "Что-то пошло не так, попробуйте снова": console.log('Ошибка сервера');
-                break;
-            }
+        const user = {
+            login: loginInput.value,
+            password: passwordInput.value,
+            passwordConfirm: passwordConfirm.value
+        }
+        let checkPassword = validatePassword(user.password) === validatePassword(user.passwordConfirm);
+        const regOptions = createOptions(user);
+        if(validateLogin(user.login) && checkPassword){
+            postRequest(REGURL, regOptions).then((data) => {
+           
+                switch (data.message) {
+                    case "Пользователь создан": window.location.pathname = "./autorizationPage.html"
+                    break;
+                    case "Такой пользователь уже существует":
+                        fieldsForErrror.innerHTML = "User already exists";
+                    break;
+                    case "Что-то пошло не так, попробуйте снова": console.log('Ошибка сервера');
+                    break;
+                }
         }).catch(() => {
-            // Здесь делать отрисовку ошибки что сервер не работает, попробуйте позже
-            console.log('Умер')
+            fieldsForErrror.innerHTML = "Server is down, try again later";
         })
+        } else{
+            fieldsForErrror.innerHTML = "Data entered incorrectly";
+         }
     })
-
 }
 regInit()
